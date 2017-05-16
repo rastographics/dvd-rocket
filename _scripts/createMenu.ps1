2684299377953fedfee6c00d68a77dd2f400a7f7
Joel Szwarga <rastographics@gmail.com>
2017-05-16 11:54:34 -0500

    experiment with getting menu to work

--- _scripts/createMenu.ps1
+++ _scripts/createMenu.ps1
@@ -2,4 +2,4 @@ $jpgFile = Get-ChildItem  -Filter *.jpg | Select-Object -First 1
 
 $strFileName = $jpgFile.Name
 
-& "bin\ffmpeg.exe" -loop 1 -i $strFileName -r 30 -frames:v 300 -vcodec mpeg2video -b 800k  -s 720x480 "menu.m2v"
+& "bin\ffmpeg.exe" -loop 1 -i $strFileName -r 30 -frames:v 300 -aspect 16:9 -y -target ntsc-dvd "menu.m2v" #-vcodec mpeg2video -b 800k  -s 720x480 "menu.m2v"
